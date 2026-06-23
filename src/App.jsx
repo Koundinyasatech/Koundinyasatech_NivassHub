b467d5922d44c0fce45ec19673c9b9e27defd85d
RamBhupalReddy1616 <rambhupal.s@koundinyasatech.com>
2026-06-23 16:35:04 +0530

    login details added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import AppRoutes from "./routes/AppRoutes";
+
+// function App() {
+//   return <AppRoutes />;
+// }
+
+// export default App;
+import Sidebar from "./components/Sidebar/Sidebar";
+
+function App() {
+  return <Sidebar />;
+}
+
+export default App;
---
+++ src/components/Sidebar/sidebarData.js
@@ -0,0 +1,26 @@
+export const sidebarData = [
+  {
+    title: "Foundation",
+    path: "/foundation",
+  },
+  {
+    title: "Operational",
+    path: "/operational",
+  },
+  {
+    title: "Community",
+    path: "/community",
+  },
+  {
+    title: "Finance",
+    path: "/finance",
+  },
+  {
+    title: "Intelligence",
+    path: "/intelligence",
+  },
+  {
+    title: "System",
+    path: "/system",
+  },
+];
